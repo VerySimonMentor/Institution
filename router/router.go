93b4c18b207f6774eaa29b545e7704e92c81b7d2
dongyifeng <verysimon@mentor.cn>
2024-04-05 17:12:30 +0800

    fix gin css

--- router/router.go
+++ router/router.go
@@ -13,7 +13,8 @@ import (
 func RouterInit(config *config.Config) *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	ginRouter := gin.Default()
-	ginRouter.LoadHTMLGlob("html/*")
+	ginRouter.LoadHTMLGlob("html/*.html")
+	ginRouter.Static("/static", "html/static")
 
 	ginRouter.GET("/", func(ctx *gin.Context) {
 		ctx.Redirect(http.StatusFound, "/login")
