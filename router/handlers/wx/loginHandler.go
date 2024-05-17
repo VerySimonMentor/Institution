69ba811b3f154850fce9715898fc66a5006da809
VerySimonMentor <152454901+VerySimonMentor@users.noreply.github.com>
2024-05-17 14:15:59 +0800

    Develop login (#27)
    * add log
    
    * add log
    
    * add log
    
    * add log
    
    * develop wx
    
    * develop login
    
    * develop login
    
    * develop login
    
    ---------
    
    Co-authored-by: dongyifeng <verysimon@mentor.cn>

--- router/handlers/wx/loginHandler.go
+++ router/handlers/wx/loginHandler.go
@@ -4,14 +4,31 @@ import (
 	"Institution/config"
 	"Institution/logs"
 	"Institution/mysql"
+	"Institution/redis"
+	"Institution/router/handlers/CRUD"
+	"context"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func FastLoginHandler(ctx *gin.Context, wxConfig *config.WxConfig) {
+	loginCode := ctx.Query("loginCode")
+	sessionKey := code2Session(wxConfig, loginCode)
+	if sessionKey == "" {
+		logs.GetInstance().Logger.Errorf("get session key error")
+		ctx.JSON(http.StatusBadRequest, gin.H{})
+	}
+	md5Hash := md5.Sum([]byte(sessionKey))
+	loginTocken := hex.EncodeToString(md5Hash[:])
+	redisClient := redis.GetClient()
+
 	code := ctx.Query("code")
 	phoneNumber := GetPhoneNumber(code, wxConfig)
 	if phoneNumber == "" {
@@ -19,17 +36,35 @@ func FastLoginHandler(ctx *gin.Context, wxConfig *config.WxConfig) {
 		ctx.JSON(http.StatusBadRequest, gin.H{})
 	}
 
+	redisClient.Set(context.Background(), loginTocken, phoneNumber, loginTockenExpireTime*time.Hour*24)
+
 	mysqlClient := mysql.GetClient()
-	var user mysql.UserSQL
-	result := mysqlClient.Where("userNumber = ?", phoneNumber).First(&user)
+	var userSQL mysql.UserSQL
+	result := mysqlClient.Where("userNumber = ?", phoneNumber).First(&userSQL)
 	if result.Error != nil {
 		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			user.UserNumber = phoneNumber
-			user.UserLevel = 0
-			if err := mysqlClient.Create(&user).Error; err != nil {
+			userSQL.UserNumber = phoneNumber
+			userSQL.UserLevel = 0
+			if err := mysqlClient.Create(&userSQL).Error; err != nil {
 				logs.GetInstance().Logger.Errorf("create user error %s", err)
 				ctx.JSON(http.StatusBadRequest, gin.H{})
 			}
+			if err := mysqlClient.Last(&userSQL).Error; err != nil {
+				logs.GetInstance().Logger.Errorf("get user info error %s", err)
+				ctx.JSON(http.StatusBadRequest, gin.H{})
+			}
+
+			user := CRUD.User{
+				UserId:       userSQL.UserId,
+				UserNumber:   userSQL.UserNumber,
+				UserLevel:    userSQL.UserLevel,
+				UserAccount:  userSQL.UserAccount,
+				UserPassWd:   userSQL.UserPassWd,
+				UserEmail:    userSQL.UserEmail,
+				StudentCount: userSQL.StudentCount,
+			}
+			userByte, _ := json.Marshal(user)
+			redisClient.RPush(context.Background(), "user", userByte)
 		} else {
 			logs.GetInstance().Logger.Errorf("get user info error %s", result.Error)
 			ctx.JSON(http.StatusBadRequest, gin.H{})
@@ -37,22 +72,24 @@ func FastLoginHandler(ctx *gin.Context, wxConfig *config.WxConfig) {
 	}
 
 	var loginState int
-	if user.UserLevel == 0 {
+	if userSQL.UserLevel == 0 {
 		loginState = 1
 	} else {
 		loginState = 2
 	}
 	ctx.JSON(http.StatusOK, gin.H{
-		"loginState": loginState,
+		"loginState":  loginState,
+		"loginTocken": loginTocken,
 	})
 }
 
 type LoginForm struct {
 	PhoneNumber string `json:"phoneNumber"`
 	Password    string `json:"password"`
+	LoginCode   string `json:"loginCode"`
 }
 
-func LoginHandler(ctx *gin.Context) {
+func LoginHandler(ctx *gin.Context, wxConfig *config.WxConfig) {
 	var loginForm LoginForm
 	if err := ctx.ShouldBindJSON(&loginForm); err != nil {
 		logs.GetInstance().Logger.Errorf("bind json error %s", err)
@@ -78,6 +115,17 @@ func LoginHandler(ctx *gin.Context) {
 		return
 	}
 
+	sessionKey := code2Session(wxConfig, loginForm.LoginCode)
+	if sessionKey == "" {
+		logs.GetInstance().Logger.Errorf("get session key error")
+		ctx.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	md5Hash := md5.Sum([]byte(sessionKey))
+	loginTocken := hex.EncodeToString(md5Hash[:])
+	redisClient := redis.GetClient()
+	redisClient.Set(context.Background(), loginTocken, loginForm.PhoneNumber, loginTockenExpireTime*time.Hour*24)
+
 	var loginState int
 	if user.UserLevel == 0 {
 		loginState = 1
@@ -85,6 +133,39 @@ func LoginHandler(ctx *gin.Context) {
 		loginState = 2
 	}
 	ctx.JSON(http.StatusOK, gin.H{
-		"loginState": loginState,
+		"loginState":  loginState,
+		"loginTocken": loginTocken,
 	})
+}
+
+func CheckLoginTockenHandler(ctx *gin.Context, wxConfig *config.WxConfig) {
+	loginTocken := ctx.Query("loginTocken")
+	if CheckLoginTocken(wxConfig, loginTocken) {
+		redisClien := redis.GetClient()
+		phone, err := redisClien.Get(context.Background(), loginTocken).Result()
+		if err != nil {
+			logs.GetInstance().Logger.Errorf("get phone number error %s", err)
+			ctx.JSON(http.StatusBadRequest, gin.H{})
+			return
+		}
+
+		mysqlClient := mysql.GetClient()
+		var user mysql.UserSQL
+		mysqlClient.Where("userNumber = ?", phone).First(&user)
+		var loginState int
+		if user.UserLevel == 0 {
+			loginState = 1
+		} else {
+			loginState = 2
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"state":      true,
+			"loginState": loginState,
+		})
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{
+			"state":      false,
+			"loginState": 0,
+		})
+	}
 }
--- router/handlers/wx/wxAPI.go
+++ router/handlers/wx/wxAPI.go
@@ -10,6 +10,8 @@ import (
 	"io"
 	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 type AccessToken struct {
@@ -19,7 +21,7 @@ type AccessToken struct {
 
 func GetAccessToken(wxConfig *config.WxConfig) string {
 	redisClient := redis.GetClient()
-	accessToken, err := redisClient.Get(context.Background(), tokenKey).Result()
+	accessToken, err := redisClient.Get(context.Background(), accessTokenKey).Result()
 	if redis.CheckNil(err) {
 		resp, e := http.Get("https://api.weixin.qq.com/cgi-bin/token?grant_type=" + wxConfig.Grant_type + "&appid=" + wxConfig.Appid + "&secret=" + wxConfig.Secret)
 		if e != nil {
@@ -31,9 +33,10 @@ func GetAccessToken(wxConfig *config.WxConfig) string {
 		body, _ := io.ReadAll(resp.Body)
 		var token AccessToken
 		json.Unmarshal(body, &token)
+		logs.GetInstance().Logger.Infof("token %+v", token)
 
 		accessToken = token.AccessToken
-		redisClient.Set(context.Background(), tokenKey, accessToken, 7200*time.Second)
+		redisClient.Set(context.Background(), accessTokenKey, accessToken, 7200*time.Second)
 	} else if err != nil {
 		logs.GetInstance().Logger.Errorf("get access token error %s", err)
 		return ""
@@ -55,9 +58,6 @@ type PhoneInfo struct {
 
 func GetPhoneNumber(code string, wxConfig *config.WxConfig) string {
 	accessToken := GetAccessToken(wxConfig)
-	if accessToken == "" {
-		return ""
-	}
 
 	requestJSON, _ := json.Marshal(map[string]string{
 		"code": code,
@@ -72,11 +72,13 @@ func GetPhoneNumber(code string, wxConfig *config.WxConfig) string {
 	body, _ := io.ReadAll(resp.Body)
 	var phoneResp PhoneNumberResp
 	json.Unmarshal(body, &phoneResp)
-	if phoneResp.ErrCode == 40001 {
+	logs.GetInstance().Logger.Infof("phone number %+v", phoneResp)
+	if phoneResp.ErrCode != 0 {
 		redisClient := redis.GetClient()
-		redisClient.Del(context.Background(), tokenKey)
+		redisClient.Del(context.Background(), accessTokenKey)
 		accessToken = GetAccessToken(wxConfig)
 		if accessToken == "" {
+			logs.GetInstance().Logger.Errorf("get access token error")
 			return ""
 		}
 		resp, err = http.Post("https://api.weixin.qq.com/wxa/business/getuserphonenumber?access_token="+accessToken, "application/json", bytes.NewBuffer(requestJSON))
@@ -88,10 +90,68 @@ func GetPhoneNumber(code string, wxConfig *config.WxConfig) string {
 
 		body, _ = io.ReadAll(resp.Body)
 		json.Unmarshal(body, &phoneResp)
+		logs.GetInstance().Logger.Infof("phone number %+v", phoneResp)
 	}
 
 	if phoneResp.PhoneInfo.CountryCode == 86 {
 		return phoneResp.PhoneInfo.PurePhoneNumber
 	}
 	return phoneResp.PhoneInfo.PhoneNumber
+}
+
+func CheckTokenHandler(ctx *gin.Context) {
+	tocken := ctx.Query("token")
+	redisClient := redis.GetClient()
+	_, err := redisClient.Get(context.Background(), tocken).Result()
+	if redis.CheckNil(err) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"state": false,
+		})
+	} else if err != nil {
+		logs.GetInstance().Logger.Infof("redis get state tocken err %v", err)
+		ctx.JSON(http.StatusBadRequest, gin.H{})
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"state": true,
+	})
+}
+
+type SessionForm struct {
+	SessionKey string `json:"session_key"`
+	Errcode    int    `json:"errcode"`
+}
+
+func code2Session(wxConfig *config.WxConfig, code string) string {
+	resp, err := http.Get("https://api.weixin.qq.com/sns/jscode2session?appid=" + wxConfig.Appid + "&secret=" + wxConfig.Secret + "&js_code=" + code + "&grant_type=authorization_code")
+	if err != nil {
+		logs.GetInstance().Logger.Errorf("get session key error %s", err)
+		return ""
+	}
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+	var session SessionForm
+	json.Unmarshal(body, &session)
+	if session.Errcode != 0 {
+		logs.GetInstance().Logger.Errorf("get session key error %v", session)
+		return ""
+	}
+
+	return session.SessionKey
+}
+
+func CheckLoginTocken(wxConfig *config.WxConfig, loginTocken string) bool {
+	redisClient := redis.GetClient()
+	exists, err := redisClient.Exists(context.Background(), loginTocken).Result()
+	if err != nil {
+		logs.GetInstance().Logger.Errorf("check session key error %s", err)
+		return false
+	}
+
+	if exists == 0 {
+		return false
+	}
+
+	return true
 }
